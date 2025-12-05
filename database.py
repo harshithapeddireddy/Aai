97f7d9165964e9b4dcdb79430bfa9ce683232906
Guanru5686 <u5686343@live.warwick.ac.uk>
2025-12-05 14:48:08 +0000

    Add files via upload

---
+++ database.py
@@ -0,0 +1,157 @@
+import pandas as pd
+import sqlite3
+
+#Create database
+conn = sqlite3.connect("Student_wellbeing.db")
+cursor = conn.cursor()
+
+#Read xlsx
+file_path = "Database.xlsx"
+df1 = pd.read_excel(file_path, sheet_name=0, usecols=[0, 1, 2], skiprows=1)
+df2 = pd.read_excel(file_path, sheet_name=0, usecols=[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], skiprows=1)
+df3 = pd.read_excel(file_path, sheet_name=0, usecols=[2, 13, 14, 15, 16, 17, 18], skiprows=1)
+df4 = pd.read_excel(file_path, sheet_name=0, usecols=[2, 19], skiprows=1)
+df5 = pd.read_excel(file_path, sheet_name=0, usecols=[2, 20, 21, 22], skiprows=1)
+
+# -----STUDENT TABLE------
+create_students_table = """
+CREATE TABLE IF NOT EXISTS Students 
+(
+    Student_ID INTEGER PRIMARY KEY,
+    Gender VARCHAR(20),
+    Student_Name VARCHAR(50)
+);
+"""
+
+# -----ATTENDANCE TABLE------
+create_attendance_table = """
+CREATE TABLE IF NOT EXISTS Attendance 
+(
+    Attendance_ID INTEGER PRIMARY KEY,
+    Student_ID INTEGER,
+    Weekly INTEGER,
+    Attendance_Status INTEGER,
+    FOREIGN KEY (Student_ID) REFERENCES Students(Student_ID)
+);
+"""
+
+# -----ASSIGNMENT TABLE------
+create_assignment_table = """
+CREATE TABLE IF NOT EXISTS Assignment 
+(
+    Assignment_ID INTEGER PRIMARY KEY,
+    Student_ID INTEGER,
+    Assignment_Name VARCHAR(50),
+    Submission_Week VARCHAR(30),
+    Submission_Pattern VARCHAR(50),
+    Assignment_Grade INTEGER,
+    FOREIGN KEY (Student_ID) REFERENCES Students(Student_ID)
+);
+"""
+
+# -----ASSESSMENT TABLE------
+create_assessment_table = """
+CREATE TABLE IF NOT EXISTS Assessment 
+(
+    Assessment_ID INTEGER PRIMARY KEY,
+    Student_ID INTEGER,
+    Assessment_Grade INTEGER,
+    FOREIGN KEY (Student_ID) REFERENCES Students(Student_ID)
+);
+"""
+
+# -----STRESS SURVEY TABLE------
+create_survey_table = """
+CREATE TABLE IF NOT EXISTS Survey (
+    Time_Usage_ID INTEGER PRIMARY KEY,
+    Student_ID INTEGER,
+    Study_Time INTEGER,
+    Entertainment_Time INTEGER,
+    Sleep_Time INTEGER,
+    FOREIGN KEY (Student_ID) REFERENCES Students(Student_ID)
+);
+"""
+
+cursor.execute(create_students_table)
+cursor.execute(create_attendance_table)
+cursor.execute(create_assignment_table)
+cursor.execute(create_assessment_table)
+cursor.execute(create_survey_table)
+
+# ----insert data to students table----
+for index, row in df1.iterrows():
+    student_id = row.iloc[2]
+    student_name = row.iloc[0]
+    gender = row.iloc[1]
+
+    cursor.execute('''
+    INSERT INTO Students (Student_ID, Student_Name, Gender)
+    VALUES (?, ?, ?)
+    ''', (student_id, student_name, gender))
+
+# ----insert data to attendance table----
+for index, row in df2.iterrows():
+    student_id = int(row.iloc[0])
+    for i in range(1, 11):
+        week = i
+        status = int(row.iloc[i])
+
+        cursor.execute('''
+        INSERT INTO Attendance (Student_ID, Weekly, Attendance_Status)
+        VALUES (?, ?, ?)
+        ''', (student_id, week, status))
+
+# ----insert data to assignment table----
+count = len(df3)
+for i in range(count):
+        student_id = int(df3.iloc[i, 0])
+        name_1 = 'Assignment 1'
+        week_1 = df3.iloc[i, 1]
+        pattern_1 = df3.iloc[i, 2]
+        grade_1 = int(df3.iloc[i, 3])
+
+        cursor.execute('''
+        INSERT INTO Assignment (Student_ID, Assignment_Name, Submission_Week, Submission_Pattern, Assignment_Grade)
+        VALUES (?, ?, ?, ?, ?)
+        ''', (student_id, name_1, week_1, pattern_1, grade_1))
+
+for j in range(count):
+        student_id = int(df3.iloc[j, 0])
+        name_2 = 'Assignment 2'
+        week_2 = df3.iloc[j, 4]
+        pattern_2 = df3.iloc[j, 5]
+        grade_2 = int(df3.iloc[j, 6])
+
+        cursor.execute('''
+        INSERT INTO Assignment (Student_ID, Assignment_Name, Submission_Week, Submission_Pattern, Assignment_Grade)
+        VALUES (?, ?, ?, ?, ?)
+        ''', (student_id, name_2, week_2, pattern_2, grade_2))
+
+# ----insert data to assessment table----
+for index, row in df4.iterrows():
+    student_id = int(row.iloc[0])
+    grade = int(row.iloc[1])
+
+    cursor.execute('''
+    INSERT INTO Assessment (Student_ID, Assessment_Grade)
+    VALUES (?, ?)
+    ''', (student_id, grade))
+
+# ----insert data to Survey table----
+for index, row in df5.iterrows():
+    student_id = int(row.iloc[0])
+    time = int(row.iloc[1])
+    etime = int(row.iloc[2])
+    stime = int(row.iloc[3])
+
+    cursor.execute('''
+    INSERT INTO Survey (Student_ID, Study_Time, Entertainment_Time, Sleep_Time)
+    VALUES (?, ?, ?, ?)
+    ''', (student_id, time, etime, stime))
+
+conn.commit()
+
+# close connection with database
+cursor.close()
+conn.close()
+print("All database tables have creat！")
---
+++ select data.py
@@ -0,0 +1,48 @@
+import sqlite3
+import pandas as pd
+import struct
+
+#Show all rows
+#pd.set_option('display.max_rows', None)
+#Show all cols
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+
+# connect to database
+database_path = 'Student_wellbeing.db'
+conn = sqlite3.connect(database_path)
+
+# select data from tables
+query1 = "SELECT * FROM students"
+df1 = pd.read_sql_query(query1, conn)
+
+query2 = "SELECT * FROM attendance"
+df2 = pd.read_sql_query(query2, conn)
+
+query3 = "SELECT * FROM assignment"
+df3 = pd.read_sql_query(query3, conn)
+
+query4 = "SELECT * FROM assessment"
+df4 = pd.read_sql_query(query4, conn)
+
+query5 = "SELECT * FROM survey"
+df5 = pd.read_sql_query(query5, conn)
+
+
+# close database connect
+conn.close()
+print("\nStudents Table Visible：")
+print(df1)
+
+print("\nAttendance Table Visible：")
+print(df2)
+
+print("\nAssignment Table Visible：")
+print(df3)
+
+print("\nAssessment Table Visible：")
+print(df4)
+
+print("\nSurvey Table Visible：")
+print(df5)
